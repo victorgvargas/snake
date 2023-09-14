dbac6291671a01103c0b7bbd2bed27e4a96e7d81
victorgvargas <vvargas2013@gmail.com>
2023-09-14 11:55:13 +0100

    snake moving correctly

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const canvas = document.getElementById("canvas");
 
 class Point {
-    /** 
-     * @param {number} x
-     * @param {number} y
-    */
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
+  /**
+   * @param {number} x
+   * @param {number} y
+   */
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 }
 
 /**
@@ -16,69 +16,80 @@ class Point {
  * @returns {string}
  */
 function mapKeyToDirection(key) {
-    switch (key) {
-        case "ArrowUp":
-            return "up";
-        case "ArrowLeft":
-            return "left";
-        case "ArrowRight": 
-            return "right";
-        case "ArrowDown":
-            return "down";
-        default:
-            return "";
-    }
+  switch (key) {
+    case "ArrowUp":
+      return "up";
+    case "ArrowLeft":
+      return "left";
+    case "ArrowRight":
+      return "right";
+    case "ArrowDown":
+      return "down";
+    default:
+      return "";
+  }
 }
 
 /**
  * @param {string} direction
  */
 function moveSnake(direction) {
-    if (!direction) return;
+  if (!direction) return;
 
-    switch (direction) {
-        case "up":
-            state.position.map(cell => ({x: cell.x, y: cell.y - 20}));
-            break;
-        case "left":
-            state.position.map(cell => ({x: cell.x - 20, y: cell.y}));
-            break;
-        case "right":
-            state.position.map(cell => ({x: cell.x + 20, y: cell.y}));
-            break;
-        case "down":
-            state.position.map(cell => ({x: cell.x, y: cell.y + 20}));
-            break;
-    }
-}   
+  let head = { ...state.position[0] };
+
+  switch (direction) {
+    case "up":
+      head.y -= 20;
+      break;
+    case "left":
+      head.x -= 20;
+      break;
+    case "right":
+      head.x += 20;
+      break;
+    case "down":
+      head.y += 20;
+      break;
+  }
+
+  state.position.unshift(head);
+  state.position.pop();
+}
 
 /**
  * @param {CanvasRenderingContext2D} ctx
- * @param {string} direction
+ * @param {Point} cell
  */
-function drawSnake(ctx) {
-    ctx.fillStyle = "green";
-    state.position.forEach(cell => ctx.fillRect(cell.x, cell.y, 20, 20));
+function drawSnakeCell(ctx, cell) {
+  ctx.fillStyle = "green";
+  ctx.fillRect(cell.x, cell.y, 20, 20);
 }
 
 // GAME LIFECYLE AREA
 const state = {
-    position: [{ x: 0, y: 40 }, { x: 0, y: 20 }, { x: 0, y: 0 }],
-    direction: "down"
+  position: [
+    { x: 0, y: 40 },
+    { x: 0, y: 20 },
+    { x: 0, y: 0 },
+  ],
+  direction: "down",
 };
 
 function render() {
-    if (canvas.getContext) {
-        const ctx = canvas.getContext("2d");
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
 
-        ctx.fillStyle = "black";
-        ctx.fillRect(0, 0, 400, 400);
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, 400, 400);
 
-        moveSnake();
-        drawSnake(ctx);
-    }
+    moveSnake(state.direction);
+    state.position.forEach((cell) => drawSnakeCell(ctx, cell));
+  }
 }
 
-document.addEventListener('keydown', (event) => mapKeyToDirection(event.key));
+document.addEventListener("keydown", (event) => {
+  state.direction = mapKeyToDirection(event.key);
+});
 
-render();
+setInterval(render, 300);
